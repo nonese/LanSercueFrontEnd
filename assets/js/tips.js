bd220b5061c34994bbf7b1b750ccb30fe42f7262
nonese <nonese@cankbird.com>
2020-11-13 01:24:16 +0800

    add tip

---
+++ assets/js/tips.js
@@ -0,0 +1,64 @@
+var uuid =localStorage.getItem("uuid"||'[]');
+$.post("http://192.168.2.220:8080/tip/get",
+{
+    uuid:uuid,
+},
+function(data,status){
+    var datas=JSON.parse(data);
+    if(datas.status == "success"){
+        console.log(data);
+        innerHTMl(datas.data);
+    }
+    else{
+        console.log(data);
+    }
+});
+function addtip(element){
+    console.log($("#content").val())
+    $.post("http://192.168.2.220:8080/tip/add",
+    {
+        uuid:uuid,
+        username:$("#username").val(),
+        content:$("#content").val(),
+        name:$("#name").val()
+    },
+    function(data,status){
+        var datas=JSON.parse(data);
+        if(datas.status == "success"){
+            console.log(data);
+            alert("发送成功");
+        }
+        else{
+            console.log(data);
+            alert(datas.status);
+        }
+    });
+}
+function innerHTMl(data) {
+    $('#dataTable tbody').empty();
+    var json=eval(data);
+    console.log(json)
+    for(var i=0; i<json.length; i++) 
+    {
+        if(json[i].readed == "unread"){
+            var str=`<tr>
+            <td>未读</td>
+            <td>${json[i].name}</td>
+            <td>${json[i].content}</td>
+            <td>${json[i].date}</td>
+            <td><button class="btn btn-primary" type="button" onclick="">设置已读</button><button class="btn btn-primary" type="button" onclick="">设置未读</button></td>
+        </tr>`
+            $("#dataTable tbody").append(str);
+        }
+        else{
+            var str=`<tr>
+            <td>已读</td>
+            <td>${json[i].name}</td>
+            <td>${json[i].content}</td>
+            <td>${json[i].date}</td>
+            <td><button class="btn btn-primary" type="button" onclick="">设置已读</button><button class="btn btn-primary" type="button" onclick="">设置未读</button></td>
+            </tr>`
+            $("#dataTable tbody").append(str);
+        }
+    }
+};
